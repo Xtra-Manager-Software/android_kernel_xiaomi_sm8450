9aaeb6a8d7c251e58e20fba6e6270d9d0dcec511
Pavelc4 <dimzzy23@gmail.com>
2025-09-09 19:33:19 +0700

    block/elevator.c: Remove duplicate condition in elevator selection
    - Removed a duplicate `IS_ENABLED(CONFIG_MQ_KYBER_DEFAULT)` condition in `block/elevator.c`.
    - This condition was causing the `kyber` elevator to be selected twice, leading to unexpected behavior.
    
    Signed-off-by: Pavelc4 <dimzzy23@gmail.com>

--- block/elevator.c
+++ block/elevator.c
@@ -679,8 +679,6 @@ void elevator_init_mq(struct request_queue *q)
 		e = elevator_get(q, "bfq", false);
 	} else if (IS_ENABLED(CONFIG_MQ_KYBER_DEFAULT)) {
 		e = elevator_get(q, "kyber", false);
-	} else if (IS_ENABLED(CONFIG_MQ_KYBER_DEFAULT)) {
-		e = elevator_get(q, "kyber", false);
 	} else if (IS_ENABLED(CONFIG_MQ_SSG_DEFAULT)) {
 		e = elevator_get(q, "ssg", false);
 	} else if (!q->required_elevator_features)
